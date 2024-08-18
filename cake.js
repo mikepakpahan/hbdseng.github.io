ddad42348d961190617f4261d486c8ccc8430cc3
mikepakpahan <145950011+mikepakpahan@users.noreply.github.com>
2024-08-18 23:02:54 +0700

    Delete cake.js

--- cake.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".candles").click(function() {
-        
-      $(".flame").animate({"opacity": 0}, "fast");
-      $(".flame2").animate({"opacity": 0}, "fast");
-      $(".flame3").animate({"opacity": 0}, "fast");
-      $(".text").animate({"top": -150, "opacity": 1}, "fast");
-    });
-});
