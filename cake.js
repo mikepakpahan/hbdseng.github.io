c2c2bc8a931d5468ba0fa4d58722a9ed9809df63
mikepakpahan <145950011+mikepakpahan@users.noreply.github.com>
2024-08-18 23:04:15 +0700

    Add files via upload

---
+++ cake.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".candles").click(function() {
+      // Memutar audio
+      var audio = document.getElementById("blow-sound");
+      audio.play();
+
+      // Menghilangkan flame dan menampilkan teks "Happy Birthday!"
+      $(".flame").animate({"opacity": 0}, "fast");
+      $(".flame2").animate({"opacity": 0}, "fast");
+      $(".flame3").animate({"opacity": 0}, "fast");
+      $(".text").animate({"top": -150, "opacity": 1}, "fast");
+  });
+});
